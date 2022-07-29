ed91a20acecea5875642daa5b9c45f5a1005afd7
DimaForostenko <forostenkochampion@gmail.com>
2022-07-29 09:47:33 +0300

    'add counter on Redux'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import * as CounterCreators from '/study/React/redux-28-07/src/actions/counterCreators';
+import * as CounterCreators from "./actions/counterCreators";
 
-function App() {
+function App(props) {
+  const { count, step, dispatch } = props;
+  const increment = () => {
+    dispatch(CounterCreators.increment());
+  };
+  const decrement = () => {
+    dispatch(CounterCreators.decrement());
+  };
+  const handlerStep = ({target:{value}}) =>{
+    dispatch(CounterCreators.setStep(Number(value)));
+  }
+  //alert('app')
+  const { count, step, increment, decrement, setStep } = props;
+  const handlerStep = ({ target: { value } }) => { setStep(Number(value));  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <input type='number' value={step} onChange={handlerStep}/>
+      <input type="number" value={step} onChange={handlerStep} />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = ({count, step}) => ({count, step})
+
+export default connect(mapStateToProps)(App);
+const mapStateToProps = ({ count, step }) => ({ count, step });
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(CounterCreators.increment()),
+  decrement: () => dispatch(CounterCreators.decrement()),
+  setStep: (newStep) => dispatch(CounterCreators.setStep(newStep)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
